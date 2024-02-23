2b9eab99eb920237b6044a3b4bd94a1bb6ef2a9e
Gabriel L. <gabriellovato15@gmail.com>
2024-02-23 00:00:51 -0300

    Made it look a little nicer

--- marching_squares.js
+++ marching_squares.js
@@ -3,7 +3,7 @@ const Vector = p5.Vector
 const RESOLUTION = 10
 
 const offsets = [
-  [0, 0],  
+  [0, 0], 
   [1, 0],  
   [0, 1],  
   [1, 1],  
@@ -60,13 +60,15 @@ function average(v1, v2) {
 
 function draw() {
   background(50);
-
-  strokeWeight(5)
   
-  const columns = width / RESOLUTION;
-  for(let i = 0; i < columns; i++) {
-    const rows = height / RESOLUTION; 
-    for(let j = 0; j < rows; j++) {
+  stroke(220)
+  strokeWeight(2)
+  
+  for(let i = 0; i < width / RESOLUTION; i++) {
+    for(let j = 0; j < height / RESOLUTION; j++) {
+      const offsetVector     = createVector(i, j)
+      const resolutionVector = createVector(RESOLUTION, RESOLUTION)
+
       const config = 
             grid[i    ][j    ] << 0 |
             grid[i + 1][j    ] << 1 |
@@ -75,23 +77,24 @@ function draw() {
       
       const tri = triangleTable[config]
       
-      stroke(220)
+      let tris = []
       
-      for(let k = 0; k < tri.length; k += 2) {
-        let edge1 = edges[tri[k]]
-        let edge2 = edges[tri[k + 1]]
-      
-        let v1 = average(offsets[edge1[0]], offsets[edge1[1]])
-        let v2 = average(offsets[edge2[0]], offsets[edge2[1]])
-
-        const offsetVector = createVector(i, j)
-        const resolutionVector = createVector(RESOLUTION, RESOLUTION)
-
-        v1.add(offsetVector).mult(resolutionVector)
-        v2.add(offsetVector).mult(resolutionVector)
+      for(const triangle of tri) {
+        const edge = edges[triangle]
         
-        strokeWeight(2)
-        line(v1.x, v1.y, v2.x, v2.y)
+        const v = average(
+          offsets[edge[0]], offsets[edge[1]])
+        
+        v.add(offsetVector).mult(resolutionVector)
+        
+        tris = [...tris, v]
+        
+        if (tris.length == 2) {
+          line(tris[0].x, tris[0].y, 
+               tris[1].x, tris[1].y)
+          
+          tris = []
+        }
       }
     }
   }
